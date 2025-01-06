2b144221e6d772c2dd6c35757114eaf10d2b7b54
jil1710 <jil.p@simformsolutions.com>
2025-01-06 15:05:52 +0530

    Add project files.

---
+++ Controllers/TwilioController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML;
+using Twilio.Types;
+
+namespace TwilioDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class TwilioController : ControllerBase
+    {
+
+
+        private readonly ILogger<TwilioController> _logger;
+
+        public TwilioController(ILogger<TwilioController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult SendMesage(string phone)
+        {
+            TwilioClient.Init("AC40a639a7a0fcd819212d994dcdb3582d", "d30182cbb12aa189d0997f4ad7842dd4");
+
+            var message = MessageResource.Create(
+                body: "What is your name",
+                from: new PhoneNumber("+1(628)232-3896"),
+                to: new PhoneNumber(phone)
+
+                );
+
+            return StatusCode(200, new { message = message.Sid });
+        }
+
+
+        [HttpPost]
+        public IActionResult RecieveMesage([FromForm] TwilioSMS twilioSMS)
+        {
+            var msg = new MessagingResponse();
+
+            msg.Message("Thank you for replying! We will look into it.");
+
+
+
+            return Content(msg.ToString(), "application/xml");
+        }
+    }
+}
---
+++ TwilioSMS.cs
@@ -0,0 +1,8 @@
+namespace TwilioDemo
+{
+    public class TwilioSMS
+    {
+        public string From { get; set; }
+        public string Body { get; set; }
+    }
+}
